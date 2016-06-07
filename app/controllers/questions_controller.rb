a9065f60ac5639dbab686193890ea152a20b3ffd
Robert Farese <robertfarese@yahoo.com>
2016-06-07 17:49:37 -0400

    added create method to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,5 +6,16 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
-  
+
+  def create
+    @question = Question.new(question_params)
+
+    @question.save
+    redirect_to @question
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :description)
+    end
 end
