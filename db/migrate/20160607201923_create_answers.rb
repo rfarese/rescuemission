3c8547b65e8123f635049a61b73459a9ae493ef9
Robert Farese <robertfarese@yahoo.com>
2016-06-07 16:25:31 -0400

    changed answers table to have all correct attributes

--- db/migrate/20160607201923_create_answers.rb
+++ db/migrate/20160607201923_create_answers.rb
@@ -1,6 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+      t.integer :question_id, null: false
+      t.integer :user_id, null: false
+      t.text :description, null: false
+
+      t.timestamps null: false
     end
   end
 end
