3c6e88cfe30c27fa88b5f9284f79198d14169583
Robert Farese <robertfarese@yahoo.com>
2016-06-08 12:29:56 -0400

    added all relevant twitter data attributes to user db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608162132) do
+ActiveRecord::Schema.define(version: 20160608162503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,14 @@ ActiveRecord::Schema.define(version: 20160608162132) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "token"
+    t.string   "secret"
+    t.string   "profile_image"
   end
 
 end
