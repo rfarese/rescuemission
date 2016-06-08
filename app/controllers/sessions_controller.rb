2bd8b9a6162d9e8d2ceb80a07fc51939dd717dd7
Robert Farese <robertfarese@yahoo.com>
2016-06-08 12:13:37 -0400

    added signin link for twitter autho

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    raise :test
     @user = User.find_or_create_from_auth_hash(auth_hash)
     self.current_user = @user
     redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: 'sessions#create'
+  root "questions#index"
   
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   resources :questions
 end
