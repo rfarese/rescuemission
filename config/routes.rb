192a69aa49455bdbc7dda074824337e0cdf6a155
Robert Farese <robertfarese@yahoo.com>
2016-06-08 21:34:27 -0400

    Added a link to navigate back to questions index from question show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "questions#index"
-  
+
   get '/auth/:provider/callback', to: 'sessions#create'
 
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
 end
