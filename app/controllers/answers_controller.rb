c0491973038ef6238c1233f8d0ba25fe73f85149
Robert Farese <robertfarese@yahoo.com>
2016-06-09 10:35:34 -0400

    added flash notice for bad form submission

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,8 +4,13 @@ class AnswersController < ApplicationController
     question_id = params[:question_id]
 
     @question = Question.find(question_id)
-    @answer = @question.answers.create(question_id: question_id, user_id: user_id, description: answer_params)
-    redirect_to question_path(@question)
+    @answer = @question.answers.create(question_id: question_id, user_id: user_id, description: answer_params["description"])
+
+    if !@answer.valid?
+      flash[:notice] = "An Answer must be atleast 50 characters long.  Try again."
+    end
+
+    redirect_to question_path(question_id)
   end
 
   private
