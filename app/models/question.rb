ee0d8eac9a7256cef9ef41a245666ced3600919f
Robert Farese <robertfarese@yahoo.com>
2016-06-14 14:59:45 -0400

    added dependent destroy to questions relationship with answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
-  has_many :answers
+  has_many :answers, dependent: :destroy 
   validates :title, presence: true, length: { minimum: 40 }
   validates :description, presence: true, length: { minimum: 150 }
 end
