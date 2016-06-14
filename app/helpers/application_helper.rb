89f9db6bce42adbd36b157aad93ffc7ba83e412a
Robert Farese <robertfarese@yahoo.com>
2016-06-14 15:58:08 -0400

    added redcarpet gem, added use_markdown method in the application helper, added use_markdown method to render questions and answers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def use_markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :no_intra_emphasis => true)
+    markdown.render(text).html_safe
+  end
 end
