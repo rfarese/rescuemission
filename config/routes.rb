413217f5ba0e319b2f2231574f90632a952cf77e
Robert Farese <robertfarese@yahoo.com>
2016-06-16 15:41:45 -0400

    added route /sign_out and assigned it to the sessions controller sign_out method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
+  get '/sign_out', to: 'sessions#sign_out'
+
   resources :questions do
     resources :answers, only: [:create, :destroy, :update]
   end
